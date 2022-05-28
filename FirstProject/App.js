ed858b6fe75058094b7541ad7204e1459ccf3dc3
sazzad4677 <sazzad35-1898@diu.edu.bd>
2022-05-28 21:49:40 +0600

    first commit

---
+++ FirstProject/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+      <StatusBar style="auto" />
+      <Button title="tap"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
